0713c8074e32eae2193a485ef96edb6a94f04966
赵林林 <zhaolinlin10@jd.com>
2020-08-19 12:15:38 +0800

    zk

--- 103. 二叉树的锯齿形层次遍历.js
+++ 103. 二叉树的锯齿形层次遍历.js
@@ -57,34 +57,4 @@ var zigzagLevelOrder = function(root) {
     return result
 };
 
-const a  = function(root) {
-    if(root==null)
-      return []
-    var arr=[root]
-    var res=[]
-    var go=true
-    while(arr.length>0){
-      var n=arr.length
-      var now=[]
-      if(go){
-        while(n-->0){
-          var node=arr.shift()
-          now.push(node.val)
-          if(node.left!=null)arr.push(node.left)
-          if(node.right!=null)arr.push(node.right)
-        }
-        res.push(now)
-      }else{
-        while(n-->0){
-          var node=arr.pop()
-          now.push(node.val)
-          if(node.right!=null)arr.unshift(node.right)
-          if(node.left!=null)arr.unshift(node.left)
-        }
-        res.push(now)
-      }
-      go=!go
-    }
-    return res
-  };
 
